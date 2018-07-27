0335b8c6dff7f737cac38cbeb5d9c7683c058e1b
Serhii <dyadenchuks@gmail.com>
2018-07-27 16:29:18 +0300

    final commit

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://powerful-atoll-97347.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(){
